864b16af363e726a4526791e1d30e2b25a702ebc
AshiqurEmon <45551479+AshiqurEmon@users.noreply.github.com>
2021-07-05 06:23:19 -0400

    Update s3_wordlist_generator.py

--- s3_wordlist_generator.py
+++ s3_wordlist_generator.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+#syntex = python3 s3_wordlist_generator.py wordlist_name.txt
 from sys import argv
 script,f=argv
 read=open(f,'r')
 for x in read:
+	print(x,end='',file=open('aws_s3_wordlist.txt', 'a'))
 	print("-"+x,end='',file=open('aws_s3_wordlist.txt', 'a'))
 	print("."+x,end='',file=open('aws_s3_wordlist.txt', 'a'))
 	print("_"+x,end='',file=open('aws_s3_wordlist.txt', 'a'))
